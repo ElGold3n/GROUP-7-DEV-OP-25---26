0b8696c1f4576836ff83d471d532e5de157e7bed
Cameron Bruce <40741621@live.napier.ac.uk>
2025-10-27 20:23:03 +0000

    ⚙️Update run configurations: Add unit tests and Jacoco configuration for the code coverage section.

---
+++ src/test/java/com/napier/devops/AppTest.java
@@ -0,0 +1,13 @@
+package com.napier.devops;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void sampleTest() {
+        // Simple test to generate Jacoco coverage
+        assertEquals(2, 1 + 1);
+    }
+}
