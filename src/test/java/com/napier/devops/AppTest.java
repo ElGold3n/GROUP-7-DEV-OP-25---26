854c955860a7fe52fcbcd0de796af34af8424b05
whashby <848241+whashby@users.noreply.github.com>
2025-10-30 11:45:01 -0400

    Create AppTest.java

---
+++ src/test/java/com/napier/devops/AppTest.java
@@ -0,0 +1,40 @@
+package com.napier.devops;
+
+import com.napier.devops.dao.*;
+import com.napier.devops.db.Database;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+public class AppTest {
+    static App app;
+    static CountryDAO countryDAO ;
+    static CityDAO cityDAO;
+    static CapitalCityDAO capitalDAO;
+    static PopulationDAO populationDAO;
+    static LookupDAO lookupDAO;
+    static LanguageDAO languageDAO;
+
+
+
+    static void init() {
+        app = new App();
+        Database db = new Database();
+        db.connect();
+        Connection conn = db.getConnection();
+
+        // Wire DAOs
+        countryDAO = new CountryDAO(conn);
+        cityDAO = new CityDAO(conn);
+        capitalDAO = new CapitalCityDAO(conn);
+        populationDAO = new PopulationDAO(conn);
+        lookupDAO = new LookupDAO(conn);
+        languageDAO = new LanguageDAO(conn);
+    }
+
+    @Test
+    public void testApp() {
+        init();
+        app.runBatchReports(countryDAO, cityDAO, capitalDAO, populationDAO, languageDAO);
+    }
+}
