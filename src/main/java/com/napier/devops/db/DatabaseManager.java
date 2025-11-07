1cfe7a13ad4fd2c98188a54a37d27f74955353de
whashby <848241+whashby@users.noreply.github.com>
2025-11-07 15:08:39 -0400

    Update DatabaseManager.java

--- src/main/java/com/napier/devops/db/DatabaseManager.java
+++ src/main/java/com/napier/devops/db/DatabaseManager.java
@@ -5,14 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseManager {
-    private static final String URL = "jdbc:mysql://db:3306/world?useSSL=false";
-    private static final String USER = "root";
-    private static final String PASSWORD = "P@ssw0rd!";
+    private static final String DEFAULT_URL =
+            "jdbc:mysql://localhost:33060/world?allowPublicKeyRetrieval=true&useSSL=false";
+    private static final String DEFAULT_USER = "root";
+    private static final String DEFAULT_PASS = "P@ssw0rd!";
 
     /**
      * Connect using  variables.
      */
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(DEFAULT_URL, DEFAULT_USER, DEFAULT_PASS);
     }
 }
